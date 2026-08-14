582a31e3e9ee9565be9372df4aeded5d9adba050
Ranida <paounda2021@gmail.com>
2026-08-14 22:49:03 +0700

    Perf: Add gzip compression to reduce API payload sizes by 93%

--- server.js
+++ server.js
@@ -11,8 +11,10 @@ const XLSX = require('xlsx');
 const { db, audit } = require('./db');
 const A = require('./aging');
 const { generateToken, authenticateToken, requireRole } = require('./auth');
+const compression = require('compression');
 
 const app = express();
+app.use(compression());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use((req, res, next) => {
